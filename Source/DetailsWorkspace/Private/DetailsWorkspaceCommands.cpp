0397f2628684a06ac913d01bcf20565437220be7
yangrc <21751230@zju.edu.cn>
2021-12-19 13:25:47 +0800

    By default user could create 4 windows.

--- Source/DetailsWorkspace/Private/DetailsWorkspaceCommands.cpp
+++ Source/DetailsWorkspace/Private/DetailsWorkspaceCommands.cpp
@@ -6,7 +6,30 @@
 
 void FDetailsWorkspaceCommands::RegisterCommands()
 {
-	UI_COMMAND(OpenPluginWindow, "DetailsWorkspace", "Bring up DetailsWorkspace window", EUserInterfaceActionType::Button, FInputGesture());
+	UI_COMMAND(
+        OpenPluginWindow[0]
+        , "Details Workspace 1"
+        , "Bring up DetailsWorkspace window"
+        , EUserInterfaceActionType::Button
+        , FInputChord());
+	UI_COMMAND(
+        OpenPluginWindow[1]
+        , "Details Workspace 2"
+        , "Bring up DetailsWorkspace window"
+        , EUserInterfaceActionType::Button
+        , FInputChord());
+	UI_COMMAND(
+        OpenPluginWindow[2]
+        , "Details Workspace 3"
+        , "Bring up DetailsWorkspace window"
+        , EUserInterfaceActionType::Button
+        , FInputChord());
+	UI_COMMAND(
+        OpenPluginWindow[3]
+        , "Details Workspace 4"
+        , "Bring up DetailsWorkspace window"
+        , EUserInterfaceActionType::Button
+        , FInputChord());
 }
 
 #undef LOCTEXT_NAMESPACE
--- Source/DetailsWorkspace/Private/DetailsWorkspaceModule.cpp
+++ Source/DetailsWorkspace/Private/DetailsWorkspaceModule.cpp
@@ -9,56 +9,81 @@
 #include "LevelEditor.h"
 #include "SAnyObjectDetails.h"
 #include "Widgets/Docking/SDockTab.h"
-#include "ToolMenus.h"
-
-static const FName DetailsWorkspaceTabName("DetailsWorkspace");
 
 #define LOCTEXT_NAMESPACE "FDetailsWorkspaceModule"
+
+static FName GetDefaultTabID(int Index)
+{
+	const FString DetailsWorkspaceTabID("DetailsWorkspace");
+	static bool bInitialized = false;
+	static TArray<FName> Result;
+	if (!bInitialized)
+	{
+		for (int i = 0; i < DETAILS_WORKSPACE_DEFAULT_INSTANCES; i++)
+		{
+			Result.Add(FName(FString::Printf(TEXT("%s%i"), *DetailsWorkspaceTabID, i)));
+		}
+		bInitialized = true;
+	}
+	return Result[Index];
+}
 
 void FDetailsWorkspaceModule::StartupModule()
 {
 	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
-	
+
 	FDetailsWorkspaceStyle::Initialize();
 	FDetailsWorkspaceStyle::ReloadTextures();
 
 	FDetailsWorkspaceCommands::Register();
-	
+
 	PluginCommands = MakeShareable(new FUICommandList);
 
-	PluginCommands->MapAction(
-		FDetailsWorkspaceCommands::Get().OpenPluginWindow,
-		FExecuteAction::CreateRaw(this, &FDetailsWorkspaceModule::PluginButtonClicked),
-		FCanExecuteAction());
+	for (int i = 0; i < DETAILS_WORKSPACE_DEFAULT_INSTANCES; i++)
+	{
+		PluginCommands->MapAction(
+			FDetailsWorkspaceCommands::Get().OpenPluginWindow[i],
+			FExecuteAction::CreateRaw(this, &FDetailsWorkspaceModule::PluginButtonClicked, i),
+			FCanExecuteAction());
 
-	FGlobalTabmanager::Get()->RegisterNomadTabSpawner(DetailsWorkspaceTabName, FOnSpawnTab::CreateRaw(this, &FDetailsWorkspaceModule::OnSpawnPluginTab))
-		.SetDisplayName(LOCTEXT("FDetailsWorkspaceTabTitle", "DetailsWorkspace"))
-	.SetMenuType(ETabSpawnerMenuType::Hidden);
-	
+		FGlobalTabmanager::Get()->RegisterNomadTabSpawner(GetDefaultTabID(i),
+		                                                  FOnSpawnTab::CreateRaw(
+			                                                  this, &FDetailsWorkspaceModule::OnSpawnPluginTab))
+		                        .SetDisplayName(LOCTEXT("FDetailsWorkspaceTabTitle", "DetailsWorkspace"))
+		                        .SetMenuType(ETabSpawnerMenuType::Hidden);
+	}
+
 	{
 		MenuExtender = MakeShareable(new FExtender);
-		MenuExtender->AddMenuExtension("LevelEditor", 
-                                            EExtensionHook::After, 
-                                            PluginCommands, 
-                                            FMenuExtensionDelegate::CreateRaw(this, &FDetailsWorkspaceModule::AddMenuEntry));
+		MenuExtender->AddMenuExtension("LevelEditor",
+		                               EExtensionHook::After,
+		                               PluginCommands,
+		                               FMenuExtensionDelegate::CreateRaw(this, &FDetailsWorkspaceModule::AddMenuEntry));
 
 		FLevelEditorModule& LevelEditorModule = FModuleManager::LoadModuleChecked<FLevelEditorModule>("LevelEditor");
 		LevelEditorModule.GetMenuExtensibilityManager()->AddExtender(MenuExtender);
 	}
 
-	ISequencerModule &SequencerModule = FModuleManager::LoadModuleChecked<ISequencerModule>("Sequencer");
-	OnSequencerCreatedHandle = SequencerModule.RegisterOnSequencerCreated(FOnSequencerCreated::FDelegate::CreateStatic(&SAnyObjectDetails::RegisterSequencer));
+	ISequencerModule& SequencerModule = FModuleManager::LoadModuleChecked<ISequencerModule>("Sequencer");
+	OnSequencerCreatedHandle = SequencerModule.RegisterOnSequencerCreated(
+		FOnSequencerCreated::FDelegate::CreateStatic(&SAnyObjectDetails::RegisterSequencer));
 }
 
 void FDetailsWorkspaceModule::AddMenuEntry(FMenuBuilder& MenuBuilder)
 {
 	MenuBuilder.BeginSection("DetailsWorkspace", TAttribute<FText>(LOCTEXT("DetailsWorkspace", "Details Workspace")));
-	MenuBuilder.AddMenuEntry(
-		LOCTEXT("OpenDetailsWorkspace", "Open Details Workspace"),
+	MenuBuilder.AddSubMenu(
+		LOCTEXT("DetailsWorkspace", "Details Workspace"),
 		FText::GetEmpty(),
-		FSlateIcon(),
-		FUIAction(FExecuteAction::CreateRaw(this, &FDetailsWorkspaceModule::PluginButtonClicked))
-	);
+		FNewMenuDelegate::CreateLambda([this](FMenuBuilder& InMenuBuilder)
+		{
+			for (int i = 0; i < 4; i++)
+			{
+				InMenuBuilder.AddMenuEntry(
+					FDetailsWorkspaceCommands::Get().OpenPluginWindow[i]
+				);
+			}
+		}));
 	MenuBuilder.EndSection();
 }
 
@@ -71,22 +96,25 @@ void FDetailsWorkspaceModule::ShutdownModule()
 
 	FDetailsWorkspaceCommands::Unregister();
 
-	FGlobalTabmanager::Get()->UnregisterNomadTabSpawner(DetailsWorkspaceTabName);
-	
-	ISequencerModule &SequencerModule = FModuleManager::LoadModuleChecked<ISequencerModule>("Sequencer");
+	for (int i = 0; i < DETAILS_WORKSPACE_DEFAULT_INSTANCES; i++)
+	{
+		FGlobalTabmanager::Get()->UnregisterNomadTabSpawner(GetDefaultTabID(i));
+	}
+
+	ISequencerModule& SequencerModule = FModuleManager::LoadModuleChecked<ISequencerModule>("Sequencer");
 	SequencerModule.UnregisterOnSequencerCreated(OnSequencerCreatedHandle);
 }
 
 TSharedRef<SDockTab> FDetailsWorkspaceModule::OnSpawnPluginTab(const FSpawnTabArgs& SpawnTabArgs)
 {
-	return CreateDetailsWorkSpace(TEXT("Default"), true);
+	return CreateDetailsWorkSpace(SpawnTabArgs.GetTabId().ToString(), true);
 }
 
-void FDetailsWorkspaceModule::PluginButtonClicked()
+void FDetailsWorkspaceModule::PluginButtonClicked(int Index)
 {
-	FGlobalTabmanager::Get()->TryInvokeTab(DetailsWorkspaceTabName);
+	FGlobalTabmanager::Get()->TryInvokeTab(GetDefaultTabID(Index));
 }
 
 #undef LOCTEXT_NAMESPACE
-	
+
 IMPLEMENT_MODULE(FDetailsWorkspaceModule, DetailsWorkspace)
